51bdde70df98d75c7be4db7cf85a8c1d6d713293
i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>
2023-05-09 21:39:05 +0400

    Updated test to work on macOS with colima

--- postgresql_testcontainers_example/src/test/java/io/github/mfvanek/postgres/example/SimpleTest.java
+++ postgresql_testcontainers_example/src/test/java/io/github/mfvanek/postgres/example/SimpleTest.java
@@ -12,37 +12,40 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collections;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
 
-public class SimpleTest {
+class SimpleTest {
 
     @Test
     void testSimple() throws SQLException {
-        try (PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:12.3")
+        try (PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres").withTag("14.5"))
                 .withTmpFs(Collections.singletonMap("/var/lib/postgresql/data", "rw"))
                 .withDatabaseName("test_db")
                 .withUsername("test_user")
-                .withPassword("test_password")) {
-
+                .withPassword("test_password")
+                .waitingFor(Wait.defaultWaitStrategy())) {
             postgres.start();
 
-            ResultSet resultSet = performQuery(postgres, "SELECT 1");
-
-            int resultSetInt = resultSet.getInt(1);
-            Assertions.assertEquals(1, resultSetInt);
+            try (HikariDataSource dataSource = getDataSource(postgres);
+                ResultSet resultSet = performQuery(dataSource, "SELECT 1")) {
+                int resultSetInt = resultSet.getInt(1);
+                Assertions.assertEquals(1, resultSetInt);
+            }
         }
     }
 
-    protected ResultSet performQuery(JdbcDatabaseContainer<?> container, String sql) throws SQLException {
-        DataSource ds = getDataSource(container);
+    protected ResultSet performQuery(DataSource ds, String sql)
+        throws SQLException {
         Statement statement = ds.getConnection().createStatement();
         statement.execute(sql);
         ResultSet resultSet = statement.getResultSet();
-
         resultSet.next();
         return resultSet;
     }
 
-    protected DataSource getDataSource(JdbcDatabaseContainer<?> container) {
+    protected HikariDataSource getDataSource(JdbcDatabaseContainer<?> container) {
         HikariConfig hikariConfig = new HikariConfig();
         hikariConfig.setJdbcUrl(container.getJdbcUrl());
         hikariConfig.setUsername(container.getUsername());
