1680a3414203cfbc8ef2ffd45cf5dfccdfcebcbb
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2023-05-31 10:40:41 +0400

    Migrated to Gradle version catalog (#16)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,13 +17,6 @@ allprojects {
     }
 }
 
-val jsr305Version by extra { "3.0.2" }
-val postgresqlVersion by extra { "42.6.0" }
-val slf4jVersion by extra { "2.0.7" }
-val logbackVersion by extra { "1.4.7" }
-val hikariVersion by extra { "5.0.1" }
-val pgihVersion by extra { "0.9.3" }
-
 subprojects {
     apply(plugin = "java")
     apply(plugin = "jacoco")
@@ -31,11 +24,11 @@ subprojects {
     apply(plugin = "org.gradle.test-retry")
 
     dependencies {
-        implementation("com.google.code.findbugs:jsr305:$jsr305Version")
-        implementation(enforcedPlatform("org.testcontainers:testcontainers-bom:1.18.1"))
+        implementation(rootProject.libs.jsr305)
+        implementation(platform("org.testcontainers:testcontainers-bom:1.18.1"))
 
         testImplementation("org.assertj:assertj-core:3.24.2")
-        testImplementation(enforcedPlatform("org.junit:junit-bom:5.9.3"))
+        testImplementation(platform("org.junit:junit-bom:5.9.3"))
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
         testImplementation("org.junit.jupiter:junit-jupiter-api")
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
 rootProject.name = "java-experiments"
-include("console-app-example", "spring-boot-app-example", "computer-science", "spring-only-app-example")
+include("console-app-example",
+        "spring-boot-app-example",
+        "computer-science",
+        "spring-only-app-example")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("jsr305", "com.google.code.findbugs:jsr305:3.0.2")
+            library("postgresql", "org.postgresql:postgresql:42.6.0")
+            library("hikaricp", "com.zaxxer:HikariCP:5.0.1")
+            library("logback-classic", "ch.qos.logback:logback-classic:1.4.7")
+            library("slf4j-api", "org.slf4j:slf4j-api:2.0.7")
+            val pgIndexHealth = version("pg-index-health", "0.9.3")
+            library("pgIndexHealth-testStarter", "io.github.mfvanek", "pg-index-health-test-starter")
+                    .versionRef(pgIndexHealth)
+            library("pgIndexHealth-testing", "io.github.mfvanek", "pg-index-health-testing")
+                    .versionRef(pgIndexHealth)
+        }
+    }
+}
