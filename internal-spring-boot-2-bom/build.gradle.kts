838a97774d367f2f662e396e102cfc104dabd2a4
Ivan Vakhrushev <mfvanek@gmail.com>
2024-01-05 21:42:08 +0400

    Add spring-boot-gradle-plugin to the BOM (#105)

--- internal-spring-boot-2-bom/build.gradle.kts
+++ internal-spring-boot-2-bom/build.gradle.kts
@@ -12,12 +12,15 @@ javaPlatform {
 dependencies {
     api(platform(project(":internal-bom")))
     api(platform("org.springdoc:springdoc-openapi:1.7.0"))
-    api(platform("org.springframework.boot:spring-boot-dependencies:2.7.18"))
+    val spring2Version = "2.7.18"
+    api(platform("org.springframework.boot:spring-boot-dependencies:$spring2Version"))
     api(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.9"))
     api(platform("org.springframework.cloud:spring-cloud-sleuth-otel-dependencies:1.1.4"))
     api(platform("org.springframework.security:spring-security-bom:5.8.9"))
 
     constraints {
+        api("org.springframework.boot:spring-boot-gradle-plugin:$spring2Version")
+
         api("ch.qos.logback:logback-classic") {
             version {
                 strictly("1.2.13")
---
+++ internal-spring-boot-3-bom/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-platform")
+    id("java-experiments.bom-publish")
+}
+
+description = "Spring Boot 3 cumulative BOM"
+
+javaPlatform {
+    allowDependencies()
+}
+
+dependencies {
+    api(platform(project(":internal-bom")))
+    val spring3Version = "3.2.1"
+    api(platform("org.springframework.boot:spring-boot-dependencies:$spring3Version"))
+
+    constraints {
+        api("org.springframework.boot:spring-boot-gradle-plugin:$spring3Version")
+    }
+}
