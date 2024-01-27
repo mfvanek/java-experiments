a2a494aa9ce7964ee53bacde23d3f89608fcb73a
Ivan Vakhrushev <mfvanek@gmail.com>
2024-01-27 22:31:25 +0400

    Update dependencies

--- internal-bom/build.gradle.kts
+++ internal-bom/build.gradle.kts
@@ -11,9 +11,10 @@ javaPlatform {
 
 dependencies {
     api(platform("org.junit:junit-bom:5.10.1"))
-    api(platform("org.testcontainers:testcontainers-bom:1.19.3"))
-    api(platform("io.github.mfvanek:pg-index-health-bom:0.10.2"))
-    api(platform("org.mockito:mockito-bom:5.9.0"))
+    api(platform("org.testcontainers:testcontainers-bom:1.19.4"))
+    api(platform("io.github.mfvanek:pg-index-health-bom:0.10.3"))
+    api(platform("org.mockito:mockito-bom:5.10.0"))
+    api(platform("org.assertj:assertj-bom:3.25.2"))
 
     constraints {
         api("com.google.code.findbugs:jsr305:3.0.2")
@@ -21,7 +22,6 @@ dependencies {
         api("com.zaxxer:HikariCP:5.1.0")
         api("ch.qos.logback:logback-classic:1.4.14")
         api("org.slf4j:slf4j-api:2.0.11")
-        api("org.assertj:assertj-core:3.25.1")
         api("com.h2database:h2:2.2.224")
         api("javax.annotation:javax.annotation-api:1.3.2")
         api("org.threeten:threeten-extra:1.7.2")
--- internal-spring-boot-3-bom/build.gradle.kts
+++ internal-spring-boot-3-bom/build.gradle.kts
@@ -11,7 +11,7 @@ javaPlatform {
 
 dependencies {
     api(platform(project(":internal-bom")))
-    val spring3Version = "3.2.1"
+    val spring3Version = "3.2.2"
     api(platform("org.springframework.boot:spring-boot-dependencies:$spring3Version"))
 
     constraints {
