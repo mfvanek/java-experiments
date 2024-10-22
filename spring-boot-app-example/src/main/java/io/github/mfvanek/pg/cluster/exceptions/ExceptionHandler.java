d6e299d004b2555767c4d03556e7fff04d11a016
Krushit-Babariya <148842311+Krushit-Babariya@users.noreply.github.com>
2024-10-22 13:24:27 +0530

    Update ExceptionHandler.java

--- spring-boot-app-example/src/main/java/io/github/mfvanek/pg/cluster/exceptions/ExceptionHandler.java
+++ spring-boot-app-example/src/main/java/io/github/mfvanek/pg/cluster/exceptions/ExceptionHandler.java
@@ -2,39 +2,46 @@ package io.github.mfvanek.pg.cluster.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 
 @ControllerAdvice
 public class ExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException ex) {
-        return new ResponseEntity<>("Employee not found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+        String message = "Employee not found: " + ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<Object> handleInvalidUUIDFormat(IllegalArgumentException ex) {
-        return new ResponseEntity<>("Invalid UUID format: " + ex.getMessage(), HttpStatus.BAD_REQUEST);
+        String message = "Invalid UUID format: " + ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error ->
-            errors.put(error.getField(), error.getDefaultMessage())
-        );
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrors) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGlobalException(Exception ex) {
-        return new ResponseEntity<>("An unexpected error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        String message = "An unexpected error occurred: " + ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
